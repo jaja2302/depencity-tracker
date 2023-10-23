8dc1390d1721b013a32a2c3c01579e9c3833f99e
Aldimas <aldimasdearsilla@gmail.com>
2023-10-23 16:07:02 +0700

    sinkronisasi blok

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\TrackerController;
-
+use App\Http\Controllers\ValidateController;
 
 Route::get('/', [LoginController::class, 'index'])->name('index');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
@@ -16,4 +16,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/drawMaps', [TrackerController::class, 'drawMaps'])->name('drawMaps');
     Route::post('/updateUserqc', [TrackerController::class, 'updateUserqc'])->name('updateUserqc');
     Route::get('/getData', [TrackerController::class, 'getData'])->name('getData');
+    Route::get('/validateMaps', [ValidateController::class, 'mainMaps'])->name('mainMaps');
+    Route::get('/getOptValidate/{id}', [ValidateController::class, 'getOptValidate'])->name('getOptValidate');
+    Route::get('/getCoordinatesValidate/{est}', [ValidateController::class, 'getCoordinatesValidate'])->name('getCoordinatesValidate');
+    Route::post('/processValidate', [ValidateController::class, 'processValidate'])->name('processValidate');
 });
